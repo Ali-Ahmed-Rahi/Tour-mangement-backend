106b5ae2e8404cb55f8a7b5df05f163d3a69f350
rahiji9 <aliahmedrahi007@gmail.com>
2025-07-10 21:27:52 +0600

    part-2

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,32 @@
 import express from 'express';
 const app = express()
-import  {Request,Response} from 'express';
+import { Request, Response } from 'express';
+import cors from 'cors';
+import { router } from './app/routes';
 
 
-app.get("/",(req:Request , res:Response )=>{
-res.status(200).json({
-  massage:"welcome to tour ManageMent"
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import { notFound } from './app/middlewares/notFound';
+
+app.use(express.json())
+app.use(cors())
+
+
+
+app.use("/api/v1", router)
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    massage: "welcome to tour ManageMent system"
+  })
 })
-})
+
+
+// Global error handler
+app.use(globalErrorHandler)
+app.use(notFound)
+
+
 
 export default app
