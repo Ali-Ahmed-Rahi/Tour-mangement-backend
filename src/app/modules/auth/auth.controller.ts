2a08a2ce6db883cda2677d93fd606dc621dc50f0
rahiji9 <aliahmedrahi007@gmail.com>
2025-07-23 16:55:56 +0600

    part-5

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -12,53 +12,53 @@ import { envVars } from "../../config/env"
 import { JwtPayload } from "jsonwebtoken"
 import passport from "passport"
 
-const credentialsLogin=catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-      // const loginInfo = await AuthServices.credentialsLogin(req.body)
+const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  // const loginInfo = await AuthServices.credentialsLogin(req.body)
 
-    passport.authenticate("local", async (err: any, user: any, info: any) => {
+  passport.authenticate("local", async (err: any, user: any, info: any) => {
 
-        if (err) { 
-            return next(new AppError(401, err))
-        }
+    if (err) {
+      return next(new AppError(401, err))
+    }
 
-        if (!user) {
-            // console.log("from !user");
-            // return new AppError(401, info.message)
-            return next(new AppError(401, info.message))
-        }
+    if (!user) {
+      // console.log("from !user");
+      // return new AppError(401, info.message)
+      return next(new AppError(401, info.message))
+    }
 
-        const userTokens = await createUserTokens(user)
+    const userTokens = await createUserTokens(user)
 
-        // delete user.toObject().password
+    // delete user.toObject().password
 
-        const { password: pass, ...rest } = user.toObject()
-
-
-        setAuthCookie(res, userTokens)
-
-        sendResponse(res, {
-            success: true,
-            statusCode: httpStatus.OK,
-            massage: "User Logged In Successfully",
-            data: {
-                accessToken: userTokens.accessToken,
-                refreshToken: userTokens.refreshToken,
-                user: rest
-
-            },
-        })
-    })(req, res, next)
-
-    // res.cookie("accessToken", loginInfo.accessToken, {
-    //     httpOnly: true,
-    //     secure: false
-    // })
+    const { password: pass, ...rest } = user.toObject()
 
 
-    // res.cookie("refreshToken", loginInfo.refreshToken, {
-    //     httpOnly: true,
-    //     secure: false,
-    // })
+    setAuthCookie(res, userTokens)
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      massage: "User Logged In Successfully",
+      data: {
+        accessToken: userTokens.accessToken,
+        refreshToken: userTokens.refreshToken,
+        user: rest
+
+      },
+    })
+  })(req, res, next)
+
+  // res.cookie("accessToken", loginInfo.accessToken, {
+  //     httpOnly: true,
+  //     secure: false
+  // })
+
+
+  // res.cookie("refreshToken", loginInfo.refreshToken, {
+  //     httpOnly: true,
+  //     secure: false,
+  // })
 
 
 })
@@ -68,64 +68,64 @@ const credentialsLogin=catchAsync(async (req: Request, res: Response, next: Next
 
 
 // get new access token
-const getNewAccessToken=catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const refreshToken=req.cookies.refreshToken;
+const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const refreshToken = req.cookies.refreshToken;
   if (!refreshToken) {
-    throw new AppError(httpStatus.BAD_REQUEST,"No Refresh Token Received from cookies")
+    throw new AppError(httpStatus.BAD_REQUEST, "No Refresh Token Received from cookies")
   }
-  const tokenInfo= await AuthServices.getNewAccessToken(refreshToken as string)
+  const tokenInfo = await AuthServices.getNewAccessToken(refreshToken as string)
 
-setAuthCookie(res,tokenInfo)
+  setAuthCookie(res, tokenInfo)
 
-  sendResponse(res,{
-    success:true,
-    statusCode:httpStatus.OK,
-    massage:"New Access token retrieved Successfully",
-    data:tokenInfo
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    massage: "New Access token retrieved Successfully",
+    data: tokenInfo
   })
 
 })
 
 // logout
-const logout=catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+const logout = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
-   res.clearCookie("accessToken",{
-    httpOnly:true,
-    secure:false,
-    sameSite:'lax'
-   })
+  res.clearCookie("accessToken", {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'lax'
+  })
 
-   res.clearCookie("refreshToken",{
-    httpOnly:true,
-    secure:false,
-    sameSite:'lax'
-   })
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'lax'
+  })
 
-  sendResponse(res,{
-    success:true,
-    statusCode:httpStatus.OK,
-    massage:"User Logout Successfully",
-    data:null
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    massage: "User Logout Successfully",
+    data: null
   })
 
 })
 
 // password change
-const resetPassword=catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
 
-  const newPassword=req.body.newPassword;
-  const oldPassword=req.body.oldPassword;
-  const decodedToken=req.user
+  const newPassword = req.body.newPassword;
+  const oldPassword = req.body.oldPassword;
+  const decodedToken = req.user
 
- await AuthServices.resetPassword(oldPassword,newPassword,decodedToken as JwtPayload)
+  await AuthServices.resetPassword(oldPassword, newPassword, decodedToken as JwtPayload)
 
 
-  sendResponse(res,{
-    success:true,
-    statusCode:httpStatus.OK,
-    massage:"Password changed Successfully",
-    data:null
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    massage: "Password changed Successfully",
+    data: null
   })
 
 })
@@ -133,32 +133,32 @@ const resetPassword=catchAsync(async (req: Request, res: Response, next: NextFun
 
 
 // google callback
-const googleCallbackController=catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+const googleCallbackController = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
-  let redirectTo=req.query.state ? req.query.state as string : "" 
+  let redirectTo = req.query.state ? req.query.state as string : ""
 
   if (redirectTo.startsWith("/")) {
-    redirectTo=redirectTo.slice(1)
+    redirectTo = redirectTo.slice(1)
   }
 
-  const user=req.user
-  console.log("this is user used to google",user);
+  const user = req.user
+  console.log("this is user used to google", user);
 
   if (!user) {
-    throw new AppError(httpStatus.NOT_FOUND,"user not found")
+    throw new AppError(httpStatus.NOT_FOUND, "user not found")
   }
-  const tokenInfo= createUserTokens(user)
+  const tokenInfo = createUserTokens(user)
 
-  setAuthCookie(res,tokenInfo)
+  setAuthCookie(res, tokenInfo)
 
 
- res.redirect(`${envVars.FRONTEND_URL}/${redirectTo}`)
+  res.redirect(`${envVars.FRONTEND_URL}/${redirectTo}`)
 })
 
 
 
 
-export const AuthControllers={
+export const AuthControllers = {
   credentialsLogin,
   getNewAccessToken,
   logout,
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { UserRoutes } from "../modules/user/user.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
+import { TourRoutes } from "../modules/tour/tour.route";
 
 export const router = Router()
 
@@ -12,7 +13,11 @@ const moduleRoutes = [
   {
     path: '/auth',
     route: AuthRoutes
-  }
+  },
+    {
+        path: "/tour",
+        route: TourRoutes
+    }
 ]
 
 
